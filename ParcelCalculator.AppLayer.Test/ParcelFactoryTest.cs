9198237a17c8e0e3a075a1a92aa8bc3d0c875096
j.lobren <jeffrey.lobren@learningseat.com>
2018-07-02 05:56:21 +0800

    Added test

---
+++ ParcelCalculator.AppLayer.Test/ParcelFactoryTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParcelCalculator.Domain.Parcel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParcelCalculator.AppLayer.Test
+{
+    [TestClass]
+    public class ParcelFactoryTest
+    {
+        [TestMethod]
+        public void ParcelFactory_GetParcel_Rejected()
+        {
+            var parcelFactory = new ParcelFactory();
+
+            var result = parcelFactory.GetParcel(51, 10, 10, 10);
+
+            Assert.IsInstanceOfType(result, typeof(RejectParcel));
+        }
+
+        [TestMethod]
+        public void ParcelFactory_GetParcel_Heavy()
+        {
+            var parcelFactory = new ParcelFactory();
+
+            var result = parcelFactory.GetParcel(41, 10, 20, 10);
+
+            Assert.IsInstanceOfType(result, typeof(HeavyParcel));
+        }
+
+        [TestMethod]
+        public void ParcelFactory_GetParcel_Medium()
+        {
+            var parcelFactory = new ParcelFactory();
+
+            var result = parcelFactory.GetParcel(9, 10, 10, 23);
+
+            Assert.IsInstanceOfType(result, typeof(MediumParcel));
+        }
+
+        [TestMethod]
+        public void ParcelFactory_GetParcel_Small()
+        {
+            var parcelFactory = new ParcelFactory();
+
+            var result = parcelFactory.GetParcel(10, 1, 3, 5);
+
+            Assert.IsInstanceOfType(result, typeof(SmallParcel));
+        }
+
+        [TestMethod]
+        public void ParcelFactory_GetParcel_Large()
+        {
+            var parcelFactory = new ParcelFactory();
+
+            var result = parcelFactory.GetParcel(8, 100, 80, 120);
+
+            Assert.IsInstanceOfType(result, typeof(LargeParcel));
+        }
+    }
+}
---
+++ ParcelCalculator.AppLayer.Test/ValidatorTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParcelCalculator.AppLayer.Test
+{
+    [TestClass]
+    public class ValidatorTest
+    {
+        [TestMethod]
+        public void Validator_CheckValues_Pass()
+        {
+            var validator = new Validator();
+            var expected = new ValidatorResult() { IsValid = true, Error = null };
+
+            var result = validator.CheckValues("12", "1", "10", "20");
+
+            Assert.AreEqual(expected.IsValid, result.IsValid);
+        }
+
+        [TestMethod]
+        public void Validator_CheckValues_Fail_Aphanumeric()
+        {
+            var validator = new Validator();
+            var expected = new ValidatorResult() { IsValid = false, Error = "The following values are invalid: Height" };
+
+            var result = validator.CheckValues("12", "A1", "10", "20");
+
+            Assert.AreEqual(expected.IsValid, result.IsValid);
+            Assert.AreEqual(expected.Error, result.Error);
+        }
+
+        [TestMethod]
+        public void Validator_CheckValues_Fail_HasDecimal()
+        {
+            var validator = new Validator();
+            var expected = new ValidatorResult() { IsValid = false, Error = "The following values are invalid: Weight" };
+
+            var result = validator.CheckValues("12.1", "1", "10", "20");
+
+            Assert.AreEqual(expected.IsValid, result.IsValid);
+            Assert.IsNotNull(expected.Error, result.Error);
+        }
+    }
+}
